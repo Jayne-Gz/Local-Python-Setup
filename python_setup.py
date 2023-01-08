79ac98471b49b5df192cb7f81f0beffdccd9f862
Angelyka (Aj) Gonzalez <105693867+Jayne-Gz@users.noreply.github.com>
2023-01-08 14:37:00 -0800

    Add files via upload

---
+++ python_setup.py
@@ -0,0 +1,15 @@
+def hello():
+    print("hello there")
+
+def pack(1, 2, 3):
+    return[1, 2 3]
+
+def eat_lunch("apples", "bananas", "carrots"):
+    if len(apples, bananas, carrots:) == 0: 
+        print("My lunchbox is empty!")
+        else:
+            for i in range(len(apples, bananas, carrots)):
+                if i == 0: 
+                    print(f"First I eat {apples, bananas, carrots[0]}")
+                else:
+                    print(f"Next I eat {apples, bananas, carrots[0]}")
